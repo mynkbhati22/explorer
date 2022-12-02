de1a6502dfb0af62b85350fef9e7455bf1130d3e
rituknj <55534242+saurabh931@users.noreply.github.com>
2022-12-02 13:43:11 +0530

    navbar

--- src/Component/Pages/Navbaroffcanvas.js
+++ src/Component/Pages/Navbaroffcanvas.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import Button from "@mui/material/Button";
@@ -11,11 +11,20 @@ import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
 import { AiOutlineMenu } from "react-icons/ai";
+import transactionsvg from "../Images/transaction.svg";
+import blocksvg from "../Images/block.svg";
+import polygonsvg from "../Images/polygon.svg";
+import { IoMdArrowDropdown } from "react-icons/io";
+import { FiSearch } from "react-icons/fi";
 
 
 import "./Navbaroffcanvas.css";
 
 export default function TemporaryDrawer() {
+  const [nav1, setNav1] = useState(false);
+  const [nav2, setNav2] = useState(false);
+  const [nav3, setNav3] = useState(false);
+
   const [state, setState] = React.useState({
     top: false,
     left: false,
@@ -54,13 +63,90 @@ export default function TemporaryDrawer() {
           </span>
         </li>
       </ul>
-      <ul className="sidebar-list">
-      <li>Blocks</li>
-      <li>Transactions</li>
-      <li>Polygon Edge Faucet</li>
-      <li>Tokens</li>
-      <li>Resources</li>
-      </ul>
+      <ul className="nav-list-offcanvas">
+            <li>
+              <img src={blocksvg} alt="" /> Blocks{" "}
+            </li>
+            <li>
+              <span
+                onClick={() => {
+                  setNav1(!nav1);
+                  setNav2(false);
+                  setNav3(false);
+                }}
+              >
+                <img src={transactionsvg} alt="" /> Transactions{" "}
+                <IoMdArrowDropdown />{" "}
+              </span>
+              <>
+                {nav1 ? (
+                  <ul className="transaction-list">
+                    <li>Validated</li>
+                    <li>Pending</li>
+                  </ul>
+                ) : (
+                  ""
+                )}
+              </>
+            </li>
+            <li>
+              <img src={polygonsvg} alt="" /> Polygon Edge Faucet{" "}
+            </li>
+            <li>
+              <span
+                onClick={() => {
+                  setNav2(!nav2);
+                  setNav1(false);
+                  setNav3(false);
+                }}
+              >
+                <img src={polygonsvg} alt="" /> Tokens <IoMdArrowDropdown />
+              </span>
+              <>
+                {nav2 ? (
+                  <ul className="transaction-list">
+                    <li>All</li>
+                    <li>Ether</li>
+                  </ul>
+                ) : (
+                  ""
+                )}
+              </>
+            </li>
+            <li>
+              <span
+                onClick={() => {
+                  setNav3(!nav3);
+                  setNav2(false);
+                  setNav1(false);
+                }}
+              >
+                Resources <IoMdArrowDropdown />
+              </span>
+              <>
+                {nav3 ? (
+                  <ul className="transaction-list">
+                    <li>Polygon Edge Doc</li>
+                    <li>Polygon Edge Repo</li>
+                  </ul>
+                ) : (
+                  ""
+                )}
+              </>
+            </li>
+            <li>
+              {" "}
+         {/*      <span className="search">
+                {" "}
+                <FiSearch />{" "}
+                <input
+                  type="text"
+                  className="input-search"
+                  placeholder="Search by address, token symbol, name, transaction, or block number"
+                />{" "}
+              </span> */}
+            </li>
+          </ul>
     </Box>
   );
 
